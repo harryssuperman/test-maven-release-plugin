5baa9b271e2f2363c2106ab69041221f17001627
harryssuperman <harryssuperman@hotmail.com>
2024-11-19 22:25:29 +0100

    init

---
+++ src/main/java/com/github/harryssuperman/test_maven_release_plugin/TestMavenReleasePluginApplication.java
@@ -0,0 +1,13 @@
+package com.github.harryssuperman.test_maven_release_plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMavenReleasePluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMavenReleasePluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/harryssuperman/test_maven_release_plugin/TestMavenReleasePluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.harryssuperman.test_maven_release_plugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMavenReleasePluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
